f554b2d8f4424581ffb61ded9f982a8100e54c30
root <enjulim7056@gmail.com>
2019-11-07 16:02:07 +0900

    config 수정

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -3,24 +3,16 @@ import OAuthManager from 'react-native-oauth';
 import Geocoder from 'react-native-geocoding';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_M2a6tLkLHwXfbdY5JJ9R1Psa7lckmFQ",
-  authDomain: "dateapp-web-d6a1c.firebaseapp.com",
-  databaseURL: "https://dateapp-web-d6a1c.firebaseio.com",
-  projectId: "dateapp-web-d6a1c",
-  storageBucket: "dateapp-web-d6a1c.appspot.com",
-  messagingSenderId: "301595866183",
-  appId: "1:301595866183:web:3c906cfdc7facbcd"
+    /* firebase api key 추가*/
 };
 
 const google_login_config={
-  scopes: [ 'https://www.googleapis.com/auth/drive.photos.readonly'],
-   webClientId:"607443651812-ua5p9tktt8gis09751u9pfc7mro0lepe.apps.googleusercontent.com",
+    /* google_login_config 추가*/
  offlineAccess: true
 }
 
 const twitter_login_config = {
-  TWITTER_CONSUMER_KEY: 'PLACE_YOUR_TWITTER_CONSUMER_KEY_HERE',
-  TWITTER_CONSUMER_SECRET: 'PLACE_YOUR_TWITTER_CONSUMER_SECRET_HERE'
+    /* twitter_login_config 추가*/
 };
 
   
